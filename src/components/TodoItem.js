b2ecdb2edfd63a399ef5f69e93c5fc17757db22b
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-21 14:05:43 +0300

    Update include exit when done edit functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -14,6 +14,12 @@ class TodoItem extends React.Component {
     })
   }
 
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
+
   render() {
 
     let viewMode = {}
@@ -52,6 +58,7 @@ class TodoItem extends React.Component {
             onChange={e => {
               this.props.setUpdate(e.target.value, id)
             }}
+            onKeyDown={this.handleUpdatedDone}
           />
         </li>
       <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
